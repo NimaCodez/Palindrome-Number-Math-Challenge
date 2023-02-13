47369600b8a1ad4c11e38a9c4fe40580c0936398
Nima <nimarahmati8par@gmail.com>
2023-02-13 22:40:44 +0330

    Code Edited

--- palindromeNumber.js
+++ palindromeNumber.js
@@ -1,17 +1,12 @@
 // A Positive number and ad reverse together => palindrome
 // 139 + 931 = 1070 => 1070 + 0701 = 1771
 
-const reverseNumber = (number) => {
-    return parseInt(number.toString().split("").reverse().join(""));
-}
+const reverseNumber = number => parseInt(number.toString().split("").reverse().join(""));
 
-const isOdd = (length) => {
-    if (length % 2 >= 1) return true;
-    else return false;
-}
-// 7 - 1 =6 /2 = 3
-// 1234321
-const editOddLengths = (num) => {
+const isOdd = length => (length % 2 >= 1) ? true : false;
+
+// 7 - 1 = 6/2 = 3 => Ex: editOddLengths(1234321) => 123321
+const editOddLengths = num => {
     const splitted = String(num).split("");
     const length = splitted.length;
 
@@ -24,11 +19,9 @@ const editOddLengths = (num) => {
     }
 }
 
-function getCopyOfArray(arr) {
-    return [...arr]
-}
+const getCopyOfArray = arr => [...arr]
 
-const isPalindromeNumber = (number) => {
+const isPalindromeNumber = number => {
     let splitted = String(number).split("");
     let sumLength = splitted.length;
     
@@ -40,7 +33,7 @@ const isPalindromeNumber = (number) => {
     if (part1 == part2) return true;
 }
 
-const calculateNumber = (num) => {
+const calculateNumber = num => {
     let result;
     let reverse;
 
@@ -58,4 +51,4 @@ const calculateNumber = (num) => {
     return console.log(`${num} took ${steps} steps to reach ${result} and be knows as a palindrome`);
 }
 
-console.log(calculateNumber(89)) // if you need your CPU, don't run this code and change the input 🐢!
+console.log(calculateNumber(89))
